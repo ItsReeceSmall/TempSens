a893cf15bb0b03ff20a5683ff18326e637317618
Reece Small <rsmall@salisbury6c.ac.uk>
2016-03-15 10:38:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import glob
 import time
-from lib/lcd1602 import LCD1602
-import lib/temp
+from lib.lcd1602 import LCD1602
+import lib.temp
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
